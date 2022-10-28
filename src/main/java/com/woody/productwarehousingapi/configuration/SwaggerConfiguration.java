2979c7d1e5201d786d2af19e82ad9f4a06d0fe2f
kikihayashi <r05522504@g.ntu.edu.tw>
2022-10-28 16:11:09 +0800

    2022/10/28 - 新增Swagger Api 功能

---
+++ src/main/java/com/woody/productwarehousingapi/configuration/SwaggerConfiguration.java
@@ -0,0 +1,46 @@
+package com.woody.productwarehousingapi.configuration;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+/**
+ * 參考網站：https://ithelp.ithome.com.tw/articles/10247180
+ * */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Spring Boot Mall Swagger",
+                "Swagger Api 測試",
+                "v1.0",
+                "",
+                new Contact("聯絡我", "",
+                        "test@gmail.com"),
+                "參考網站", "https://ithelp.ithome.com.tw/articles/10247180",
+                Collections.emptyList());
+    }
+}
